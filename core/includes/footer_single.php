e836d95bd90b17ebce35c49f62acbf59d57cf47c
Charly Escalona <charly.escalona1@gmail.com>
2020-08-02 15:01:53 +0200

    Ajout d'un footer pour single

---
+++ core/includes/footer_single.php
@@ -0,0 +1,8 @@
+<footer><h2>SINGLE</h2></footer>
+</main>
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':3001/livereload.js?snipver=1"></' + 'script>')</script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/turbolinks/5.2.0/turbolinks.js"></script>
+    <script src="assets/js/app.js"></script>
+</body>
+</html>
