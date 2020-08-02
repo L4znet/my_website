431611bd7e2804c629548bc9d0ba94692c56e4d6
Charly Escalona <charly.escalona1@gmail.com>
2020-08-02 15:06:56 +0200

    Création d'un header pour single

---
+++ core/includes/header_single.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title><?= $title ?>Charly Escalona</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="icon" href="assets/img/favicon.ico" />
+</head>
+<body>
